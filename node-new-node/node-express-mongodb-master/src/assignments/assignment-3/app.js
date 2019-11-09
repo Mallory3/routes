da756a500bdc14a4c6bce76054dceb50b17c02c8
Richard Roy <richard_roy@hotmail.com>
2019-11-08 17:13:52 -0700

    connected to mongodb

--- node-new-node/node-express-mongodb-master/src/assignments/assignment-3/app.js
+++ node-new-node/node-express-mongodb-master/src/assignments/assignment-3/app.js
@@ -1,16 +1,21 @@
+//bring in express
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+//require dotenv
+const dotenv = require('dotenv');
+//envoke dotenv
+dotenv.config();
 
 //add defRouter model
-const defRouter = require('./routes/definitions');
+const router = require('./routes/users');
 
 //boot express
 const app = express();
 app.set('view engine','ejs');
 //include if accepting post data in your app
 app.use(express.urlencoded({extended: true}));
+
 
 //need the mongoose code higher up, it is used to connect to db
 mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true,useNewUrlParser: true });
@@ -22,14 +27,17 @@ db.once('open', function() {
   console.log('DB Connected!!!');
 });
 
+
 //set default route to homepage, after connected to db
 app.get('/', function(request, response){
-  response.render('index');
+  response.render('users');
 })
 
-//add definition route (app.use because using router: next set homepoint and boot our route);
-app.use('/definitions', defRouter);
 
+//add user route (app.use because using router: next set homepoint and boot our route);
+app.use('/users', router)
+
+// static assets
 app.use(express.static(path.join(__dirname, 'public')));
 
 //catch all 404 errors
@@ -39,12 +47,10 @@ app.use(function(req, res, next) {
 });
 
 
-// Routes
-app.use('/', require('./routes/index.js'));
-
-
+//create a port to run app on
 const PORT = process.env.PORT || 3000;
 
+//create a server and bring in the port
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
 });
---
+++ node-new-node/node-express-mongodb-master/src/assignments/assignment-3/models/Users.js
@@ -0,0 +1,35 @@
+//This is where we are storing our mongoose model
+
+//Need to install its own dependencies because it is its own module. We dont have to re-install as we installed it for app.js
+const mongoose = require('mongoose');
+
+//This will take an object: for each will take another embedded object (object of objects)
+//defines what data is going to look like
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      require: true
+    },
+    adult: {
+      type: Boolean,
+      default: false
+
+    }
+  }
+);
+
+
+//compile model
+//mongoose will look for Users on database
+const User = mongoose.model('Users', userSchema);
+
+//Export module to export userSchema model
+module.exports = User;
+console.log("module exported")
+
+//anything you want is defined in model ie password, username, email...
